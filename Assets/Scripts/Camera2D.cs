1cd5483a2987a014a98fcae256aa60c5db27b0c8
Silvan Spross <silvan.spross@gmail.com>
2013-10-27 12:42:13 +0100

    added following 2d cam and adjusted position and player speed

---
+++ Assets/Scripts/Camera2D.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera2D : MonoBehaviour {
+	
+	public Transform target;
+	public int zOffset;
+	public int minimumHeight = 0;
+	
+	private Vector3 position;
+	private float currentY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		position = target.position;
+		position.z -= zOffset;
+		
+		currentY = target.position.y;
+		
+		if (currentY > minimumHeight) {
+			position.y = currentY;	
+		} else {
+			position.y = minimumHeight;
+		}
+		
+		transform.position = position;
+	}
+}
